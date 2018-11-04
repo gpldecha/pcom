53c29679293a10ab0d1830eb3b814de480219c45
Guillaume de Chambrier <chambrierg@gmail.com>
2018-11-04 17:23:15 +0000

    removed build, first commit

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/guillaume/cpp_work_space/pcom/test
-# Build directory: /home/guillaume/cpp_work_space/pcom/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(test_producer)
-subdirs(test_circular_buffer)
--- build/test/test_circular_buffer/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/guillaume/cpp_work_space/pcom/test/test_circular_buffer
-# Build directory: /home/guillaume/cpp_work_space/pcom/build/test/test_circular_buffer
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_circular_buffer "/home/guillaume/cpp_work_space/pcom/build/test/test_circular_buffer/test_circular_buffer")
