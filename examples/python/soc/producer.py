a076ee834eb0f788a51ef8bef3c09677b2462c10
Guillaume de Chambrier <chambrierg@gmail.com>
2020-02-06 22:11:42 +0000

    working

--- examples/python/soc/producer.py
+++ examples/python/soc/producer.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 import socket
+import struct
+import selectors
 import sys
 import traceback
 from threading import Thread
 import threading
 import queue
 import pickle
+import types
+import numpy as np
+import cv2
 
 class Publiser(threading.Thread):
 
@@ -13,46 +18,104 @@ class Publiser(threading.Thread):
         threading.Thread.__init__(self)
         self.host = host
         self.port = port
-        self.queue = queue.Queue(maxsize=2)
+        self.queues = dict()
+        self.deamon=True
         self.start()
 
     def send(self, data):
-        if self.queue.qsize() >= 2:
-            self.queue.get()
-        self.queue.put(data)
+        if len(self.queues) == 0: return
+        for _, q in self.queues.items():
+            if q.qsize() >= 2:
+                q.get()
+            q.put(data)
+
+    def accept_wrapper(self, sel, sock):
+        conn, addr = sock.accept()  # Should be ready to read
+        ip, port = addr
+        print('accepted connection from', addr)
+        conn.setblocking(False)
+        data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        sel.register(conn, events, data=data)
+        try:
+            self.queues[ip + ':' + str(port)] = queue.Queue(maxsize=2)
+            Thread(target=self._client_thread, args=(conn, ip, port)).start()
+        except:
+            print('Unable to start thread')
+
+    def service_connection(self, sel, key, mask):
+        sock = key.fileobj
+        data = key.data
+        if mask & selectors.EVENT_READ:
+            try:
+                recv_data = sock.recv(1024)  # Should be ready to read
+                if recv_data:
+                    data.outb += recv_data
+                else:
+                    print('closing connection to', data.addr)
+                    ip, port = data.addr
+                    self.queues.pop(ip + ':' + str(port), None)
+                    sel.unregister(sock)
+                    sock.close()
+                    print('Remaining clients connected')
+                    for key in self.queues:
+                        print(key)
+            except ConnectionResetError as e:
+                print('service connection excepion: {}'.format(e))
+                print('closing connection to', data.addr)
+                ip, port = data.addr
+                self.queues.pop(ip + ':' + str(port), None)
+                sel.unregister(sock)
+                sock.close()
+                print('Remaining clients connected')
+                for key in self.queues:
+                    print(key)
+
+        if mask & selectors.EVENT_WRITE:
+            if data.outb:
+                print('echoing', repr(data.outb), 'to', data.addr)
+                sent = sock.send(data.outb)  # Should be ready to write
+                data.outb = data.outb[sent:]
+
 
     def run(self):
-        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        soc.bind((self.host, self.port))
-        soc.listen(5)
+        sel = selectors.DefaultSelector()
+        pub_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        pub_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        pub_socket.bind((self.host, self.port))
+        pub_socket.setblocking(0)
+        pub_socket.listen(5)
+        sel.register(pub_socket, selectors.EVENT_READ, data=None)
+
         while True:
-            connection, address = soc.accept()
-            ip, port = str(address[0]), str(address[1])
-            print("Connected with " + ip + ":" + port)
-            try:
-                Thread(target=self._client_thread, args=(connection, ip, port)).start()
-            except:
-                print("Thread did not start.")
-        soc.close()
+            events = sel.select(timeout=None)
+            for key, mask in events:
+                if key.data is None:
+                    self.accept_wrapper(sel, key.fileobj)
+                else:
+                    self.service_connection(sel, key, mask)
 
     def _client_thread(self, connection, ip, port):
         is_active=True
+        q = self.queues[ip + ":" + str(port)]
         while is_active:
             try:
-                data = self.queue.get()
-                msg = pickle.dumps(data)
-                connection.sendall(msg)
+                data = q.get()
+                payload = pickle.dumps(data)
+                header = struct.pack("L", len(payload))
+                message = header + payload
+                connection.sendall(message)
             except:
                 connection.close()
-                print("Connection " + ip + ":" + port + " closed")
                 is_active = False
+
 
 if __name__ == "__main__":
     import time
-    publisher = Publiser('localhost', 8000)
+    publisher = Publiser('localhost', 8001)
+    cap = cv2.VideoCapture(0)
     i=0
     while True:
-        publisher.send({'data': i})
+        ret, frame = cap.read()
+        publisher.send({'time': i, 'image': frame})
         i += 1
-        time.sleep(0.001)
